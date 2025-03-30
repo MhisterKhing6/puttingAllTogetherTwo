515dfd8c0dbbfdec64d27eea1862fff7428930fd
Kinglsey <kinglsey@localhost.localdomain>
2025-03-30 19:42:28 +0100

    chore:done

--- src/main/java/com/example/picturegaller/config/DatabaseConfig.java
+++ src/main/java/com/example/picturegaller/config/DatabaseConfig.java
@@ -20,7 +20,7 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
-        String url = "jdbc:mysql://" + parameterstoreUtil.getParameter("/weekfiveproject/db/host", false) + ":"  + parameterstoreUtil.getParameter("/weekfiveproject/db/port", false) +  parameterstoreUtil.getParameter("/weekfiveproject/db/name", false);
+        String url = "jdbc:mysql://" + parameterstoreUtil.getParameter("/weekfiveproject/db/host", false) + ":/"  + parameterstoreUtil.getParameter("/weekfiveproject/db/port", false) +  parameterstoreUtil.getParameter("/weekfiveproject/db/name", false);
         dataSource.setJdbcUrl(url); //parameterstoreUtil.getParameter("/myapp/db/url", false));
         dataSource.setUsername(parameterstoreUtil.getParameter("/weekfiveproject/db/username", false));
         dataSource.setPassword(parameterstoreUtil.getParameter("/weekfiveproject/db/password", true));
